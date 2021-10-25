1ccbbadc877f946e64cafb4072e3109b8b0303e9
Dominik Lovric <dlovric@ucsd.edu>
2021-10-24 17:10:14 -0700

    Finished Assignment

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -4,4 +4,62 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+    var pressToTalk = document.querySelector("button");
+	var textToSpeak = document.getElementById("text-to-speak");
+	var smileyFace = document.querySelector("#explore > img");
+	var voiceSelect = document.getElementById("voice-select");
+
+	var synthesis = window.speechSynthesis;
+
+	var voices = [];
+
+	function populateVoiceList() {
+		voices = synthesis.getVoices();
+		for (var i = 0; i < voices.length; i++) {
+			var option = document.createElement("option");
+			option.textContent = voices[i].name + " (" + voices[i].lang + ")";
+
+			if (voices[i].default) {
+				option.textContent += " -- DEFAULT";
+			}
+
+			option.setAttribute("data-lang", voices[i].lang);
+			option.setAttribute("data-name", voices[i].name);
+			voiceSelect.appendChild(option);
+		}
+	}
+
+	populateVoiceList();
+
+	if (synthesis.onvoiceschanged !== undefined) {
+		synthesis.onvoiceschanged = populateVoiceList;
+	}
+
+	pressToTalk.addEventListener("click", function (event) {
+		smileyFace.src = `assets/images/smiling-open.png`;
+		var message = new SpeechSynthesisUtterance(textToSpeak.value);
+		var selectedOption = voiceSelect.selectedOptions[0].getAttribute(
+			"data-name"
+		);
+
+
+
+		for (var i = 0; i < voices.length; i++) {
+			if (voices[i].name === selectedOption) {
+				message.voice = voices[i];
+			}
+		}
+		message.volume = 1;
+		message.rate = 1;
+		message.pitch = 1;
+
+		synthesis.speak(message);
+		console.log(synthesis.speaking);
+
+		message.addEventListener('end', function(event) {
+			smileyFace.src = `assets/images/smiling.png`;
+		});
+
+		textToSpeak.blur();
+	});
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,63 @@
 // expose.js
-
+const jsConfetti = new JSConfetti();
+var horn_select = document.getElementById("horn-select");
+var volume_controls = document.querySelector("#volume-controls > input");
+var hornImage = document.querySelector("#expose > img");
+var volumeImage = document.querySelector("#volume-controls > img");
+var changeAudio = document.querySelector("#expose > audio");
+var honk_button = document.querySelector("#expose > button");
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  honk_button.addEventListener('click', playAudio);
+  function playAudio(audio){
+    audio.preventDefault();
+    if (volume_controls.volume != 0 && horn_select.value == "party-horn"){
+      jsConfetti.addConfetti();
+    }
+    honk_button.setAttribute("disabled", "");  
+    changeAudio.play();
+    changeAudio.addEventListener('ended', function(event) {
+      honk_button.removeAttribute("disabled", "");  
+    });
+  }
+
+  volume_controls.addEventListener('input',changeVolume);
+  function changeVolume(){
+    changeAudio.volume = volume_controls.value / 100;
+    if(volume_controls.value == 0){
+      volumeImage.src = "./assets/icons/volume-level-0.svg";
+      honk_button.setAttribute("disabled", "");
+    }
+    else{
+      if(volume_controls.value > 0 && volume_controls.value <= 33){
+        volumeImage.src = "./assets/icons/volume-level-1.svg";
+      }
+      else if(volume_controls.value > 33 && volume_controls.value < 67){
+        volumeImage.src = "./assets/icons/volume-level-2.svg";
+      }
+      else if(volume_controls.value >= 67){
+        volumeImage.src = "./assets/icons/volume-level-3.svg";
+      }
+      honk_button.removeAttribute("disabled", "");
+    }
+  }
+
+  horn_select.addEventListener('change', changeHorn);
+  function changeHorn(){
+    if(horn_select.value == "air-horn"){
+      changeAudio.src = "./assets/audio/air-horn.mp3";
+      hornImage.src = "./assets/images/air-horn.svg";
+    }
+    else if(horn_select.value == "car-horn"){
+      changeAudio.src = "./assets/audio/car-horn.mp3";
+      hornImage.src = "./assets/images/car-horn.svg";
+    }
+    else{
+      changeAudio.src = "./assets/audio/party-horn.mp3";
+      hornImage.src = "./assets/images/party-horn.svg";
+    }
+  }
 }
